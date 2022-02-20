29b0bf3b9ded3eb7ebe3f1c63c3290892e6b6b2f
Schalk <schalkolivier@vegamine.com>
2022-02-20 11:43:54 +0200

    Updated view of preek in django admin

--- genesis/backend/admin.py
+++ genesis/backend/admin.py
@@ -4,4 +4,6 @@ from backend.models import Prediker, Gemeente, Reeks, Preek
 admin.site.register(Prediker)
 admin.site.register(Gemeente)
 admin.site.register(Reeks)
-admin.site.register(Preek)
+@admin.register(Preek)
+class PreekAdmin(admin.ModelAdmin):
+    list_display = ('datum', 'gemeente', 'prediker', 'tema', 'downloads')
