780bcd6c55cd2948b53d712b19935a251db745ee
Schalk <schalkolivier@vegamine.com>
2022-03-21 00:28:37 +0200

    Fixed bug on protected route that only allowed it to be used for the login page

--- exodus/frontend/src/components/ProtectedRoute/ProtectedRoute.js
+++ exodus/frontend/src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import StoreContext from "../../context/StoreContext";
 
-const ProtectedRoute = ({ user, children }) => {
+const ProtectedRoute = ({ user, page, children }) => {
     const { raiseError } = useContext(StoreContext);
 
     useEffect(() => {
@@ -10,15 +10,23 @@ const ProtectedRoute = ({ user, children }) => {
             raiseError(
                 "Meld asseblief aan om toegang tot hierdie bladsy te kry"
             );
-        } else if (!user.permissions.includes("sermon_admin")) {
-            raiseError("Jy het nie toestemming om hierdie bladsy te beskou nie")
+        } else if (
+            page === "new_sermon" &&
+            !user.permissions.includes("sermon_admin")
+        ) {
+            raiseError(
+                "Jy het nie toestemming om hierdie bladsy te beskou nie"
+            );
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     if (!user) {
         return <Navigate to="/inteken" replace />;
-    } else if (!user.permissions.includes("sermon_admin")) {
+    } else if (
+        page === "new_sermon" &&
+        !user.permissions.includes("sermon_admin")
+    ) {
         return <Navigate to="/" replace />;
     }
 
