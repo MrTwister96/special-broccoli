af5cd17e94b95835106d20bc1fff8fee6cbfacf8
Schalk <schalkolivier@vegamine.com>
2022-03-21 00:24:58 +0200

    checking custom permissions on Navbar and protected routes

--- exodus/frontend/src/components/ProtectedRoute/ProtectedRoute.js
+++ exodus/frontend/src/components/ProtectedRoute/ProtectedRoute.js
@@ -10,12 +10,16 @@ const ProtectedRoute = ({ user, children }) => {
             raiseError(
                 "Meld asseblief aan om toegang tot hierdie bladsy te kry"
             );
+        } else if (!user.permissions.includes("sermon_admin")) {
+            raiseError("Jy het nie toestemming om hierdie bladsy te beskou nie")
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     if (!user) {
         return <Navigate to="/inteken" replace />;
+    } else if (!user.permissions.includes("sermon_admin")) {
+        return <Navigate to="/" replace />;
     }
 
     return children;
